56543eb822b6ffbbe27472cbdf5ff73c99de269a
Abdul Samad M J <samadmj4@gmail.com>
2024-09-19 17:33:06 +0400

    socket io plugin integrated

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  io: {
+    enabled: true,
+    config: {
+      // Listens for changes in the `product` content type
+      contentTypes: ["api::product.product"], // Adjust this to your content type
+      // Additional configurations (CORS, etc.)
+      ios: {
+        cors: {
+          origin: "*", // Adjust the origin as per your frontend URL in production
+          methods: ["GET", "POST"],
+        },
+      },
+    },
+  },
+});
